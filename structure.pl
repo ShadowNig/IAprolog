d9dfedb9bfb3c3d1fa6f8382e26460ddeab584fa
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2019-10-31 11:01:55 -0300

    fix next relation

--- structure.pl
+++ structure.pl
@@ -1,8 +1,7 @@
-@@ -0,0 +1,51 @@
 %%A struct will be a list of 9 positions, with the numbers 1 to 8 and blank
 
-position(X,[X|L], 0) :- !.
-position(X,[_|L],Z) :- position(X,L,Y), Z is Y+1,!.
+position(X,[X|_], 0).
+position(X,[_|L],Z) :- position(X,L,Y), Z is Y+1.
 
 isCorner(X,L) :- position(X,L,0),!.
 isCorner(X,L) :- position(X,L,2),!.
@@ -18,29 +17,44 @@ isCenter(X,L) :- position(X,L,4),!.
 
 
 neighboard(0,1).
+neighboard(1,0).
+
 neighboard(0,3).
+neighboard(3,0).
 
 neighboard(1,2).
+neighboard(2,1).
+
 neighboard(1,4).
+neighboard(4,1).
 
 neighboard(2,5).
+neighboard(5,2).
 
 neighboard(3,4).
+neighboard(4,3).
+
 neighboard(3,6).
+neighboard(6,3).
 
 neighboard(4,5).
+neighboard(5,4).
+
 neighboard(4,7).
+neighboard(7,4).
 
 neighboard(5,8).
+neighboard(8,5).
 
 neighboard(6,7).
+neighboard(7,6).
 
 neighboard(7,8).
+neighboard(8,7).
 
-neighboard(X,Y) :- X < Y, neighboard(Y,X).
 
-substitute(_,[],_,[]).
-substitute(0,[_|L], V, [V|L]).
+substitute(_,[],_,[]) :- !.
+substitute(0,[_|L], V, [V|L]):- !.
 substitute(I,[X|L], V, [X|K]) :- J is I-1, substitute(J,L,V,K).
 
 swap(I,J,L1, L3) :- position(V1,L1,J),position(V2,L1,I),substitute(I,L1,V1,L2),substitute(J,L2,V2,L3).
